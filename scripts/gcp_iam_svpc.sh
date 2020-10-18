5de0630887b500dee67e1b8687298fb1d51d127f
Wolfpacker <ncsu.ee2017@gmail.com>
2020-10-18 15:51:39 -0400

    Delete gcp_iam_svpc.sh

--- scripts/gcp_iam_svpc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TF_VAR_org_id=$(gcloud organizations list --format='value(name)' --limit=1)
-GOOGLE_ACCOUNT=$(gcloud config get-value account)
-
-gcloud beta organizations add-iam-policy-binding ${TF_VAR_org_id} \
-  --member user:${GOOGLE_ACCOUNT} \
-  --role roles/compute.xpnAdmin
